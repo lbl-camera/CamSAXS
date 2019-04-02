f699de179febaa35f3489b35ce674baf080b31f2
Ronald Pandolfi <ronpandolfi@gmail.com>
2019-04-02 12:02:25 -0700

    Make test pytestable

---
+++ tests/test_remesh.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+"""
+
+import camsaxs
+import numpy as np
+from pyFAI import detectors, geometry
+import matplotlib.pylab as plt
+import timeit
+
+def func(args):
+    camsaxs.remesh(*args)
+
+def test_remesh():
+    filename = 'GIWAXS_sfloat_2m.gb'
+    center = [632., 69.5]
+    pixel = [172E-06, 172E-06]
+    poni = np.multiply(center, pixel)
+    dist = 300.
+    det = detectors.Pilatus2M
+    g = geometry.Geometry(dist=0.3, poni1=poni[1], poni2=poni[0],
+                pixel1=pixel[0], pixel2=pixel[1], wavelength=0.123984E-09)
+
+    data = np.fromfile(filename, dtype=np.float32).reshape(1679,1475)
+    data = np.flipud(data)
+    ai = np.deg2rad(0.14)
+    lims = [[0, 20],[0, 20]]
+    res = [512, 512]
+
+
+    # default qp-qz, same size as detector
+    img, x, y = camsaxs.remesh(data, g, ai)
+    box = [x.min(), x.max(), y.min(), y.max()]
+    plt.imshow(np.log(img+4), origin='lower', extent=box)
+    plt.title('qp_qz_full.png')
+    plt.show()
+
+    plt.figure()
+    img, x , y = camsaxs.remesh(data, g, ai, res=res, coord_sys='qp_qz', out_range=lims)
+    box = [x.min(), x.max(), y.min(), y.max()]
+    plt.imshow(np.log(img+4), origin='lower', extent=box)
+    plt.title('qp_qz_roi.png')
+    plt.show()
+
+    plt.figure()
+    img, x , y = camsaxs.remesh(data, g, ai, coord_sys='qy_qz')
+    box = [x.min(), x.max(), y.min(), y.max()]
+    plt.imshow(np.log(img+4), origin='lower', extent=box)
+    plt.title('qy_qz_full.png')
+    plt.show()
+
+    plt.figure()
+    img, x , y = camsaxs.remesh(data, g, ai, res=res, coord_sys='qy_qz')
+    box = [x.min(), x.max(), y.min(), y.max()]
+    plt.imshow(np.log(img+4), origin='lower', extent=box)
+    plt.title('qy_qz_roi.png')
+    plt.show()
+
+    plt.figure()
+    img, x , y = camsaxs.remesh(data, g, ai, coord_sys='theta_alpha')
+    box = [x.min(), x.max(), y.min(), y.max()]
+    plt.imshow(np.log(img+4), origin='lower', extent=box)
+    plt.title('th_al_full.png')
+    plt.show()
+
+    plt.figure()
+    lims = [[-0.1, 0.1],[0, 0.2]]
+    res = [2000, 2000]
+    img, x , y = camsaxs.remesh(data, g, ai, res=res, coord_sys='theta_alpha', out_range=lims)
+    box = [x.min(), x.max(), y.min(), y.max()]
+    plt.imshow(np.log(img+4), origin='lower', extent=box)
+    plt.title('th_al_roi.png')
+    plt.show()
+
+if __name__=='__main__':
+    test_remesh()
