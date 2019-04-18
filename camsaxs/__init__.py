c9ea2709c77c2e49fd0befd402a12a7ae2ef8a56
dkumar <dkumar@lbl.gov>
2019-04-18 11:08:19 -0700

    1. Wrpped load_models in try, except block TODO: yaml loader has changed, in how it interpret tags. Need to update the loader.py

--- camsaxs/__init__.py
+++ camsaxs/__init__.py
@@ -8,4 +8,9 @@ from .fit_sasmodel import fit_sasmodel
 
 # load sasmodels
 from .loader import load_models
-models = load_models()
+try:
+    models = load_models()
+except:
+    print('Warning: failed to load SASMODLES.')
+    print('Please consider submitting a but-report') 
+
